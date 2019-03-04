f1c12ce2d2d3f01ae0bb2f64459bdd9beefbed82
interview <interview@baozhun.com>
2019-03-04 16:30:58 +0800

    7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,24 @@
+import java.security.GeneralSecurityException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd");
         Message message = new Message();
+        MailService mailService = new MailService();
         String text = message.readTxtFile("C:\\Users\\jumbo\\Desktop\\employee birthday\\src\\main\\resources\\employee_records.txt");
-        message.sendBirthGreeting(text);
+        Arrays.stream(text.split("\n")).forEach(s -> {
+            if (s.split(",")[2].split("/")[1].equals(simpleDateFormat.format(date).split("-")[0])) {
+                try {
+                    mailService.sendMail(s.split(",")[0], s.split(",")[3]);
+                } catch (GeneralSecurityException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
 
